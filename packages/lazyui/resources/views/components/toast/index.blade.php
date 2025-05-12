433565a041d1aa2c68db709661519b7e5112927c
ilsyaa <ilham.shindou@gmail.com>
2025-05-12 16:19:51 +0700

    chore: build vite

--- packages/lazyui/resources/views/components/toast/index.blade.php
+++ packages/lazyui/resources/views/components/toast/index.blade.php
@@ -6,19 +6,19 @@
 
 @php
     $classPlacement = [
-        'top-right' => 'right-0 top-0 sm:mt-6 sm:mr-6',
-        'top-left' => 'left-0 top-0 sm:mt-6 sm:ml-6',
-        'top-center' => 'left-1/2 -translate-x-1/2 top-0 sm:mt-6',
-        'bottom-right' => 'right-0 bottom-0 sm:mb-6 sm:mr-6',
-        'bottom-left' => 'left-0 bottom-0 sm:mb-6 sm:ml-6',
-        'bottom-center' => 'left-1/2 -translate-x-1/2 bottom-0 sm:mb-6',
+        'top-right' => 'sm:right-4 sm:-translate-x-0 top-4 right-1/2 translate-x-1/2',
+        'top-left' => 'sm:left-4 sm:-translate-x-0 top-4 left-1/2 -translate-x-1/2',
+        'top-center' => 'left-1/2 -translate-x-1/2 top-5',
+        'bottom-right' => 'sm:right-4 sm:-translate-x-0 bottom-4 right-1/2 translate-x-1/2',
+        'bottom-left' => 'sm:left-4 sm:-translate-x-0 bottom-4 left-1/2 -translate-x-1/2',
+        'bottom-center' => 'left-1/2 -translate-x-1/2 bottom-5',
     ]
 @endphp
 
 <div
     {{
         $attributes
-            ->twMerge([ 'fixed w-full z-[99] sm:max-w-2xs', $classPlacement[$placement] ?? $classPlacement['top-right'] ])
+            ->twMerge([ 'fixed w-full z-[99] max-w-[95%] sm:max-w-2xs', $classPlacement[$placement] ?? $classPlacement['top-right'] ])
     }}
     data-lazy-toast='@json(["placement" => $placement, "max" => $max, "duration" => $duration])'
     aria-label="LazyToast Notification"
