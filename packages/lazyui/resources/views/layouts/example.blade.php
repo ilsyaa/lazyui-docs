e90dd34042ddaa1580cc55b9716077f2558da8d5
ilsyaa <ilham.shindou@gmail.com>
2025-05-22 16:53:31 +0700

    refactor: change icons fontawesome pro -> sonar

--- packages/lazyui/resources/views/layouts/example.blade.php
+++ packages/lazyui/resources/views/layouts/example.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
     <div class="lazy-container-sm">
-        <div class="text-center">Hello World</div>
+        <div class="flex flex-col gap-3 justify-center items-center text-center h-[calc(100svh-200px)]">
+            <div class="text-sm font-medium">Welcome LazyUI</div>
+            <div class="text-xs text-cat-500">resouces/views/[path]/index.blade.php</div>
+        </div>
     </div>
 @endsection
--- resources/views/components/nav/item.blade.php
+++ resources/views/components/nav/item.blade.php
@@ -15,7 +15,7 @@
                 ->twMerge('relative flex justify-start items-center py-[11.5px] px-3.5 text-sm font-medium rounded-lg focus:outline-none w-full text-cat-600 dark:text-cat-500 hover:bg-cat-300/20 dark:hover:bg-cat-600/10 transition-colors ease-in-out [.active_&]:!text-accent-500 [.active_&]:!dark:text-accent-300 [.active_&]:!bg-accent-500/10 [.active_&]:!dark:bg-accent-400/10 [.active_&]:!hover:bg-accent-400/20 [&[disabled]]:!select-none [&[disabled]]:!text-cat-500 [&[disabled]]:dark:!text-cat-600 [&[disabled]]:pointer-events-none');
         }}
     >
-        <div class="mr-3 [&>svg]:size-4.5 [&>svg]:text-center flex-shrink-0">
+        <div class="mr-3 [&>svg]:size-5.5 [&>svg]:text-center flex-shrink-0">
             {{ $icon }}
         </div>
         @isset ($description)
